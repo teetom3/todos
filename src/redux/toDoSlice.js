094fa39dbad4b7ec75da2e3e1760ace8fa486244
thomas <thomas.zeryouh@goons.fr>
2025-06-26 10:11:49 +0200

    act 13

--- src/redux/toDoSlice.js
+++ src/redux/toDoSlice.js
@@ -8,18 +8,14 @@ const todoSlice = createSlice({
   reducers: {
     // Action pour ajouter une tâche
     addTodo: (state, action) => {
-      return [
-        ...state,
-        { id: Date.now(), text: action.payload, completed: false },
-      ];
+      state.push({ id: Date.now(), text: action.payload, completed: false });
     },
     // Action pour marquer une tâche comme terminée
     toggleTodo: (state, action) => {
-      return state.map((todo) =>
-        todo.id === action.payload
-          ? { ...todo, completed: !todo.completed }
-          : todo
-      );
+      const todo = state.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
     },
   },
 });
